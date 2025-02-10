019f125bfbe0dabd4c6cdcefeb178061b6a00d03
Bruno Carreira <brunocarreira816@gmail.com>
2025-02-09 22:49:12 -0300

    alteração feita com sucesso

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,7 +5,7 @@
 
             <p class="cabecalho-lg frase-cabecalho">
                 <span class="texto-verde">Um banquete de ideias para</span>
-            despertar o chef que há em você. Alteração Teste002
+            despertar o chef que há em você. Alteração Lançada Com Sucesso
             </p>
 
             <p class="subtitulo-lg">
